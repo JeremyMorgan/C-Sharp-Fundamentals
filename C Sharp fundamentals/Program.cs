5b743fb41e31871ddee7ade4ece787c97ca666ce
Jeremy Morgan <jeremymorgan@gmail.com>
2015-04-16 13:31:35 -0700

    Start Fresh

--- C Sharp fundamentals/Program.cs
+++ C Sharp fundamentals/Program.cs
@@ -5,37 +5,7 @@ namespace C_Sharp_fundamentals
 {
     class Program
     {
-        private static void WriteBytes(float value)
-        {
-            byte[] bytes = BitConverter.GetBytes(value);
-            WriteByteArray(bytes);
-        }
-
-        private static void WriteBytes(int value)
-        {
-            byte[] bytes = BitConverter.GetBytes(value);
-            WriteByteArray(bytes);
-        }
-
-        private static void WriteNames(params string[] names)
-        {
-
-            foreach (string name in names)
-            {
-                Console.Write(name);
-            }
-        }
-
-        private static void WriteByteArray(byte[] bytes)
-        {
-            foreach (byte b in bytes)
-            {
-                Console.Write("0x{0:X} {1} {2} ", b, "1", "2");
-
-            }
-        }
-
-
+   
         static void Main(string[] args)
         {
 
@@ -45,75 +15,16 @@ namespace C_Sharp_fundamentals
             book.AddGrade(75f);
 
             GradeStatistics stats = book.ComputeStatistics();
-            book.NameChanged += OnNameChanged;
-          
-            //book.NameChanged = new NameChangedDelegate(onNameChanged);
+                  
 
-
-
-            book.Name = "Jeremy's Book";
-
-            WriteNames(book.Name);
-
-            //Console.WriteLine(stats.AverageGrade);
-            //Console.WriteLine(stats.HighestGrade);
-            //Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
 
             Console.ReadLine();
         }
 
-        private static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine("Name Changed from {0} to {1}", args.OldValue, args.NewValue);
-        }
-
-        static void GiveBookAName(GradeBook book)
-        {
-            book.Name = "The GradeBook";
-        }
-
-        static void IncrementNumber(ref int number)
-        {
-            number += 1;
-        }
-
-        private static void Immutable()
-        {
-
-        }
-
-        private static void Arrays()
-        {
-            float[] grades;
-            grades = new float[3];
-
-            AddGrades(grades);
-
-            foreach (float grade in grades)
-            {
-                Console.WriteLine(grade);
-
-            }
-
-        }
-
-        private static void AddGrades(float[] grades)
-        {
-            grades[0] = 91f;
-            grades[1] = 89.1f;
-            grades[2] = 75f;
-
-        }
-
-        private static void PassByValueAndRef()
-        {
-            GradeBook g1 = new GradeBook();
-            GradeBook g2 = g1;
-
-            GiveBookAName(g1);
-
-            Console.WriteLine(g2.Name);
-        }
+    
     }
 
 
