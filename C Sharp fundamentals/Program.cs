1cd5b91ba1bcaa85213fa488fefb439907ba61f3
Jeremy Morgan <jeremymorgan@gmail.com>
2015-04-15 18:21:17 -0700

    Bytes Excercise

--- C Sharp fundamentals/Program.cs
+++ C Sharp fundamentals/Program.cs
@@ -5,6 +5,36 @@ namespace C_Sharp_fundamentals
 {
     class Program
     {
+        private static void WriteBytes(float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteBytes(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteNames(params string[] names)
+        {
+
+            foreach (string name in names)
+            {
+                Console.Write(name);
+            }
+        }
+
+        private static void WriteByteArray(byte[] bytes)
+        {
+            foreach (byte b in bytes)
+            {
+                Console.Write("0x{0:X} {1} {2} ", b, "1", "2");
+
+            }
+        }
+
 
         static void Main(string[] args)
         {
@@ -16,97 +46,20 @@ namespace C_Sharp_fundamentals
 
             GradeStatistics stats = book.ComputeStatistics();
 
+            WriteBytes(stats.AverageGrade);
+            WriteNames("Jeremy", "test", "cool", "yeah");
 
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.LowestGrade);
 
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
-
-
-            //Arrays();
-
-
-
-            /*
-             * 
-            const int numberOfStudents = 4;
-
-            int[] scores = new int[numberOfStudents];
-
-            int totalScore = 0;
-
-            scores[0] = 3;
-            scores[1] = 7;
-            scores[2] = 21;
-            scores[3] = 33;
-
-
-            foreach (int score in scores)
-            {
-
-                totalScore += score;
-            }
-
-            double averageScore = (double)totalScore / scores.Length;
-
-            Console.WriteLine("Average: " + averageScore);
-
-            //Immutable();
-
-            /*
-            string name = " Jeremy ";
-            
-            name = name.Trim();
-
-            DateTime date = new DateTime(2002,1,1);
-
-            date.AddHours(25);
-            
-            
-            Console.WriteLine(date);
-
-
-
-
-            /*
-            DateTime date = new DateTime(2014, 9, 11);
-
-            date.AddDays(3);
-
-            Console.WriteLine(date);
-
-
-
-            PassByValueAndRef();
-            
-            /*
-            int x1 = 4;
-            int x2 = x1;
-
-            x1 = 100;
-
-            Console.WriteLine("value of x " + x2);
-
-            /*
-            GradeBook book = new GradeBook();
-
-            book.AddGrade(91);
-            book.AddGrade(23);
-            book.AddGrade(88);
-            book.AddGrade(92);
-
-            GradeStatistics stats = book.ComputeStatistics();
-
-            Console.WriteLine("Average Grade: " + stats.AverageGrade);
-            Console.WriteLine("Lowest Grade: " + stats.LowestGrade);
-            Console.WriteLine("Highest Grade: " + stats.HighestGrade);
-
-            */
 
             Console.ReadLine();
 
 
         }
+
+
 
         static void GiveBookAName(GradeBook book)
         {
@@ -120,7 +73,7 @@ namespace C_Sharp_fundamentals
 
         private static void Immutable()
         {
-      
+
         }
 
         private static void Arrays()
@@ -147,10 +100,10 @@ namespace C_Sharp_fundamentals
         }
 
 
-       
 
-      
-     
+
+
+
 
         private static void PassByValueAndRef()
         {
